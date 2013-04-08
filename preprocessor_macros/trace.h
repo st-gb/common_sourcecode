cb806ce4ab022f8e92c3f7d7078fe41a21209aff
Stefan Gebauer <hypoaktiv@yahoo.de>
2013-04-08 21:07:41 +0200

    added copyright info

--- preprocessor_macros/trace.h
+++ preprocessor_macros/trace.h
@@ -1,3 +1,10 @@
+/* Do not remove this header/ copyright information.
+ *
+ * Copyright © Trilobyte Software Engineering GmbH, Berlin, Germany 2010-2011.
+ * You are allowed to modify and use the source code from
+ * Trilobyte Software Engineering GmbH, Berlin, Germany for free if you are not
+ * making profit with it or its adaption. Else you may contact Trilobyte SE.
+ */
 /*
  * trace.h
  *
